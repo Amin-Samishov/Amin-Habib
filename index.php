f1972f0ea25bb80db51d9b2abd78c6d8d785fe05
Habib <khabba13@yandex.ru>
2022-01-26 13:00:40 +0500

    Add function makeMilkshakeWithStrawberries

--- index.php
+++ index.php
@@ -5,4 +5,29 @@
     2. Strawberries - 200 gram
     3. Honey - 1 tablespoon
 */
+
+    function makeMilkshakeWithStrawberries
+    (   int $milk,
+        int $strawberries,
+        int $honey,
+        string $good_words
+    )
+        {
+            echo 
+            "1. Put the $strawberries gram strawberries and $honey tablespoon honey in a blender;<br>".
+            "2. Pureeing until smooth;<br>".
+            "3. Add the $milk ml milk to the strawberry mass and whisk thoroughly again;<br>".
+            $good_words;
+
+            /*
+                How to make a milkshake with strawberries:
+                1. Put strawberries and honey in a blender.
+                2. Pureeing until smooth.
+                3. Add the milk to the strawberry mass and beat thoroughly again.
+                4. That's it - a milkshake with strawberries is ready. 
+                5. Pour the cocktail into glasses and serve, preferably with a straw.
+            */
+        }
+
+    makeMilkshakeWithStrawberries(200, 200, 1, "Enjoy!")
 ?>
