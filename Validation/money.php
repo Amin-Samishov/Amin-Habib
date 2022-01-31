d86fe406676a30218f33d7120b7db3851252c267
Habib <khabba13@yandex.ru>
2022-01-31 23:30:17 +0500

    Some changes and additions to the function validateTransfer

--- Validation/money.php
+++ Validation/money.php
@@ -1,6 +1,6 @@
 <?php
 
-function validateType($money_in_cents, $sender_account_number, $recipient_account_number)
+function validateInt($money_in_cents, $sender_account_number, $recipient_account_number)
 {
     echo "Sender's account number: $sender_account_number";
     echo "<br>";
@@ -15,15 +15,6 @@ function validateType($money_in_cents, $sender_account_number, $recipient_accoun
     return "OK! 200. Money in cents: ".$money_in_cents;
 }
 
-
-function validateMax($money_in_cents, $max_value)
-{
-    if ($money_in_cents >= $max_value) {
-        return "ERROR! This amount of money is a lot";
-    }
-    return "OK! 200. Money within the maximum: ".$money_in_cents;
-}
-
 function validateMin($money_in_cents, $min_value)
 {
     if ($money_in_cents < $min_value)
@@ -33,8 +24,22 @@ function validateMin($money_in_cents, $min_value)
     return "OK! 200. Money within the minimum: ".$money_in_cents;
 }
 
-echo validateType(2500000, 6085431297834617, 4158420396874917);
-echo "<br>";
-echo validateMax(9000000, 10000000);
-echo "<br>";
-echo validateMin(150000, 100000);
+function validateMax($money_in_cents, $max_value)
+{
+    if ($money_in_cents >= $max_value) {
+        return "ERROR! This amount of money is a lot";
+    }
+    return "OK! 200. Money within the maximum: ".$money_in_cents;
+}
+
+function validateTransfer($money_in_cents)
+{
+    $validate_message = "";
+    $validate_message .= validateInt($money_in_cents, 4276802156936452, 6276081265936423);
+    $validate_message .= validateMin($money_in_cents, 100000);
+    $validate_message .= validateMax($money_in_cents, 10000000);
+    return "$validate_message";
+}
+
+$money_in_cents = 250000;
+echo validateTransfer($money_in_cents);
