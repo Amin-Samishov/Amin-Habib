1dc5df633f3f99d01e0650d765441304a755625c
Amin <amin.15.07.2005@mail.ru>
2022-02-03 19:48:21 +0500

    modified4.coordinates.php

--- coordinates.php
+++ coordinates.php
@@ -1,72 +1,91 @@
 <?php
 
-function coordinates($latitude, $longitude){
-
-    $longitude = readline('Please write a longitude');
+function validateLongitudeFloat($longitude) : string
+{
+	if (!is_float($longitude)) {
+		return "ERROR: Type of longitude must be float!";
+	} else {
+		return "Type of longitude is correct! Thanks!";
+    }
     echo '<br>';
-    $latitude = readline('Please write a latitude');
-    echo coordinates( $latitude,  $longitude);
-    
-} return "ok!";
 
-function validationCount($count_of_longitude, $count_of_latitude, $longitude, $latitude ){
-    //количество  символов долготы
-    $count_of_longitude = count($longitude); 
-    $count_of_latitude = count($latitude);
-if ($count_of_longitude <= 1) {
-    return "Error, longitude too short!";
+function validateLatitudeFloat($latitude){
+    if (!is_float($latitude)) {
+		return "ERROR: Type of latitude must be float!";
+	} else {
+		return "Type of latitude is correct! Thanks!";
+    }
+    }
 }
-    return 'количество  символов долготы : ' . $count_of_longitude;
+    echo '<br>';
 
-
-echo '<br>';
-
-if ($count_of_latitude <= 1) {
-    return "Error, latitude too short!";
+function validateMin($longitude, $latitude, $min_value) : string
+{
+	if ($longitude <= $min_value) {
+		return "ERROR: Too few characters of longitude sent";
+	} else {
+		return "Min characters of longitude are OK";
+    }
+    echo '<br>';
+    if ($latitude <= $min_value) {
+		return "ERROR: Too few characters of latitude sent";
+	} else {
+		return "Min characters of latitude are OK";
+    }
 }
-    return 'количество  символов широты : ' . $count_of_longitude;
-}   //количество  символов широты
-    
 
 echo '<br>';
 
-function validateType ($type_of_longitude, $type_of_latitude, $longitude, $latitude){   
-    $type_of_longitude = gettype($longitude);
-    $type_of_latitude = gettype($latitude);    
-
-    if (!is_float($type_of_longitude)) { //проверяем тип долготы 
-        return "invalid longitude, is not a float!";
+function validateMax($longitude, $latitude, $max_value) : string
+{
+	if ($longitude >= $max_value) {
+		return "ERROR: Too much characters of longitude sent";
+	} else {
+		return "Max characters of longitude are OK";
     }
-        return $longitude;
-    
- 
- echo'<br>';
- 
-    if (!is_float($type_of_latitude)) { //проверяем тип широты
-        return "invalid latitude, is not a float!";
-    }
-        return $latitude;
-    }
-
+    echo '<br>';
+    if ($latitude >= $max_value) {
+		return "ERROR: Too much characters of latitude  sent";
+	} else {
+		return "Max characters of latitude are OK";
+	}
 echo '<br>';
 
-function checkLocation($coordinates_max, $coordinates_min , $longitude, $latitude ){
-//находится ли в моем квадарате заданные координаты?
-$coordinates_min=20.45684;
-$coordinates_max=30.45235;
 
-if ($longitude <= $coordinates_min) { 
+function validateSquareMinValue($longitude, $latitude, $x1, $y1){
+
+if ($longitude <= $x1 || $latitude<=$y1) { 
    return "error 422 coordinates are outside my square";
 }
-    return "Ok. longtitude coordinates in my square!";
-
-
-if ($latitude >= $coordinates_max ) {
-    return "error 422 coordinates are outside my square";
-}
-    return "Ok. latitude coordinates in my square!";
+    return "Ok. coordinates in my square(min value)!";
 }
 
+echo '<br>';
 
+function validateSquareMaxValue($longitude, $latitude, $x2, $y2){
+    if ($longitude >= $x2 || $latitude>=$y2 ) {
+        return "error 422 coordinates are outside my square";
+    }
+        return "Ok. coordinates in my square(max value)!";
+    }
+}
 
+function validateCoordination($longitude, $latitude){
+echo "Longitude coordination : " . $longitude;
+echo ' <br>';
+echo "Latitude coordination : " . $latitude;
+echo ' <br>';
 
+$validate_message = "";
+$validate_message .= validateLongitudeFloat($longitude);
+$validate_message .= validateLatitudeFloat($latitude);
+$validate_message .= validateMin($longitude, $latitude, 0);
+$validate_message .= validateMax($longitude, $latitude, 555.555);
+$validate_message .= validateSquareMinValue($longitude, $latitude, 122.122, 455.455);
+$validate_message .= validateMax($longitude, $latitude, 233.233, 566.566);
+return $validate_message;
+}
+
+$longitude = 123.123;
+$latitude = 456.456;
+echo validateCoordination($longitude, $latitude);
