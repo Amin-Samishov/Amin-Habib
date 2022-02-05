fee5c77e158e12d1abaecb18bd3c7c865951f501
Habib <khabba13@yandex.ru>
2022-02-05 15:42:36 +0500

    Fixed all comments errors

--- Validation/money.php
+++ Validation/money.php
@@ -19,7 +19,7 @@ function validateMin($money_in_cents, $min_value)
     return "OK! 200. Money minimum: ".$money_in_cents;
 }
 
-function validateMax($money_in_cents, $a, $max_value)
+function validateMax($money_in_cents, $max_value)
 {
     if ($money_in_cents >= $max_value) {
         return "ERROR! This amount of money is a lot";
@@ -27,33 +27,35 @@ function validateMax($money_in_cents, $a, $max_value)
     return "OK! 200. Money maximum: ".$money_in_cents;
 }
 
-function validate($rules, $request, $min_value, $max_value)
+function validate($rules, $request)
 {
     $message = [];
     $rules_in_array = explode("|", $rules);
     foreach ($rules_in_array as $rule)
     {
-        array_push($message, call_user_func_array("validate".ucfirst($rule), [$request, $min_value, $max_value]));
-
+        $value =  explode(":", $rule);
+        if ($value[0]=="int"){
+            array_push($message, call_user_func_array("validate".ucfirst($value[0]), [$request]));
+        }else{
+            array_push($message, call_user_func_array("validate".ucfirst($value[0]), [$request, $value[1]]));
+        }
     }
     return $message;
 }
 
-function index($request, $min_value, $max_value, $sender_account_number, $recipient_account_number)
+function index($request, $sender_account_number, $recipient_account_number)
 {
 
     echo "Sender's account number: $sender_account_number<br>";
     echo "Recipient's account number: $recipient_account_number<br>";
-    $rules = "int|max|min";
-    $messages = validate($rules, $request, $min_value, $max_value);
+    $rules = "int|min:100000|max:10000000";
+    $messages = validate($rules, $request);
 
     return count($messages)
         ? implode("<br>", $messages)
         : 'No Validation messages';
 }
 $request = 250000;
-$min_value = 100000;
-$max_value = 10000000;
 $sender_account_number = 4276802156936452;
 $recipient_account_number = 6276081265936423;
-echo index($request, $min_value, $max_value, $sender_account_number, $recipient_account_number );
+echo index($request, $sender_account_number, $recipient_account_number);
